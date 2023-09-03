b12042e2e998b008899a9092c13754e243ad6286
TakamizawaNoriaki <takamizawanoriaki@MacBookAirForNori.local>
2023-09-03 23:53:52 +0900

    initial commit

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name="_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column
+	private int id;
+    
+    @Column
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
